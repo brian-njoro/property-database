237333896cb15f7945294a22e60f4fca1dc16149
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2024-03-14 17:36:24 +0300

    populate newspaper table

---
+++ newspaperdata.sql
@@ -0,0 +1,9 @@
+INSERT INTO Newspaper (NewspaperName) VALUES
+('The Standard Newspaper'),
+('The Guardian'),
+('The Wall Street Journal'),
+("The People's Daily"),
+('The Times'),
+('Kenya Today'),
+('The Daily Telegraph'),
+('The Washington Post');
