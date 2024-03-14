984f99200fd48ce9a834b6854cc692781edf3175
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2024-03-14 17:55:11 +0300

    seed advertisement info

--- advertisement.sql
+++ advertisement.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Advertisement (
-    AdvertisementID INTEGER PRIMARY KEY,
+    AdvertisementID INTEGER PRIMARY KEY AUTOINCREMENT,
     PropertyNumber INTEGER,
     NewspaperName TEXT,
     DisplayDate DATE,
     FOREIGN KEY (PropertyNumber) REFERENCES Property(PropertyNumber)
 );
+-- DROP TABLE Advertisement;
---
+++ advertisementdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO Advertisement (PropertyNumber, NewspaperName, DisplayDate) VALUES
+(1, 'The Standard Newspaper', '2024-03-01'),
+(2, 'The Guardian', '2024-03-02'),
+(3, 'The Wall Street Journal', '2024-03-03'),
+(4, "The People's Daily", '2024-03-04'),
+(5, 'The Times', '2024-03-05'),
+(6, 'Kenya Today', '2024-03-06'),
+(7, 'The Daily Telegraph', '2024-03-07');
